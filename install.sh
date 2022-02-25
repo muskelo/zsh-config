5b48474acae6bf104098562eecae0fc23d72b0d0
Muskelo <titanioman@mail.ru>
2022-02-25 13:45:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,9 @@
 
 # Осторожно, можно потерять текущие конфиги
 ln -sf $HOME/.config/zsh/.zshrc $HOME/.zshrc
+
+# install oh my tmux
+if ! [ -d "$HOME/.oh-my-zsh"]
+then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
