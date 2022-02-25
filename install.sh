5edd8f2966b5f0345c3c36c7f5f6fc647f0aa4a9
Muskelo <titanioman@mail.ru>
2022-02-26 02:32:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ if ! [ -d "$HOME/.oh-my-zsh"]
 then
     echo 'Y' | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     rm "$HOME/.zshrc"
-
 fi
-
+# create symlink to .zshrc
 ln -sf $HOME/.config/zsh/.zshrc $HOME/.zshrc
+# change defualt console to zsh
+chsh -s $(which zsh)
