76d517ce9c0717fde4c79401d7fee3f79f74ccdf
Muskelo <titanioman@mail.ru>
2022-07-30 19:40:32 +0300

    add go plugin, change install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ then
     echo 'Y' | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     rm "$HOME/.zshrc"
 fi
+
 # create symlink to .zshrc
 ln -sf $HOME/.config/zsh/.zshrc $HOME/.zshrc
-# change defualt console to zsh
-chsh -s $(which zsh)
